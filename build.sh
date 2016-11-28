dd6cd3b4f5bc41099a2823dd5d779811e35c91ac
Aleksey Konovkin <alkon2000@mail.ru>
2016-11-28 15:28:21 +0300

    build refactor and add check archive

--- build.sh
+++ build.sh
@@ -11,11 +11,17 @@ build_release=1
 DIR="$(pwd)"
 
 VERSION="1.11.6"
+ZOO_VERSION="3.5.2-alpha"
+PCRE_VERSION="8.39"
+LUAJIT_VERSION="2.1.0-beta2"
+
+SUFFIX="-zoo"
 
 BASE_PREFIX="$DIR/build"
 INSTALL_PREFIX="$DIR/install"
 
 ZOO_PREFIX="$DIR/build/deps/zookeeper"
+PCRE_PREFIX="$DIR/build/pcre-$PCRE_VERSION"
 JIT_PREFIX="$DIR/build/deps/luajit"
 
 export LUAJIT_INC="$JIT_PREFIX/usr/local/include/luajit-2.1"
@@ -24,10 +30,10 @@ export LUAJIT_LIB="$JIT_PREFIX/usr/local/lib"
 export LD_LIBRARY_PATH="$JIT_PREFIX/lib:$ZOO_PREFIX/lib"
 
 function clean() {
+  rm -rf install  2>/dev/null
   if [ $download -eq 1 ]; then
     rm -rf build    2>/dev/null
     rm -rf download 2>/dev/null
-    rm -rf install  2>/dev/null
   else
     if [ $build_debug -eq 1 ] && [ $build_release -eq 1 ]; then
       if [ -e build ]; then
@@ -35,10 +41,9 @@ function clean() {
         if [ $build_deps -eq 1 ]; then
           rm -rf deps/* 2>/dev/null
         fi
-        cd nginx-$VERSION                 2>/dev/null && make clean > /dev/null 2>&1 ; cd $DIR/build
-        cd LuaJIT-*                       2>/dev/null && make clean > /dev/null 2>&1 ; cd $DIR/build
-        cd zookeeper-*/src/c              2>/dev/null && make clean > /dev/null 2>&1 ; cd $DIR/build
-        rm -rf nginx-$VERSION-*.tar.gz    2>/dev/null
+        cd nginx-$VERSION                2>/dev/null && make clean > /dev/null 2>&1 ; cd $DIR/build
+        cd LuaJIT-$LUAJIT_VERSION        2>/dev/null && make clean > /dev/null 2>&1 ; cd $DIR/build
+        cd zookeeper-$ZOO_VERSION/src/c  2>/dev/null && make clean > /dev/null 2>&1 ; cd $DIR/build
         cd ..
       fi
     fi
@@ -52,7 +57,7 @@ fi
 
 function build_zoo() {
   echo "Build Zookeeper"
-  cd zookeeper-*/src/c
+  cd zookeeper-$ZOO_VERSION/src/c
   ./configure --prefix="$ZOO_PREFIX" > /dev/null
   make -j 8 > /dev/null
   r=$?
@@ -65,7 +70,7 @@ function build_zoo() {
 
 function build_luajit() {
   echo "Build luajit"
-  cd LuaJIT-*
+  cd LuaJIT-$LUAJIT_VERSION
   make -j 8 > /dev/null
   r=$?
   if [ $r -ne 0 ]; then
@@ -76,10 +81,11 @@ function build_luajit() {
 }
 
 function build_debug() {
-  cd nginx-${VERSION}
+  cd nginx-$VERSION
   if [ $build_debug -eq 1 ] && [ $build_release -eq 1 ]; then
-    echo "Configuring debug nginx-${VERSION}"
-    ./configure --prefix="$INSTALL_PREFIX/nginx-$VERSION-zoo" \
+    echo "Configuring debug nginx-$VERSION$SUFFIX"
+    ./configure --prefix="$INSTALL_PREFIX/nginx-$VERSION$SUFFIX" \
+                --with-pcre=$PCRE_PREFIX \
                 --with-stream \
                 --with-debug \
                 --with-cc-opt="-O0 -I$ZOO_PREFIX/include" \
@@ -94,7 +100,7 @@ function build_debug() {
     exit $r
   fi
 
-  echo "Build debug nginx-${VERSION}-zoo"
+  echo "Build debug nginx-$VERSION$SUFFIX"
   make -j 8 > /dev/null
 
   r=$?
@@ -103,14 +109,15 @@ function build_debug() {
   fi
   make install > /dev/null
 
-  mv "$INSTALL_PREFIX/nginx-$VERSION-zoo/sbin/nginx" "$INSTALL_PREFIX/nginx-$VERSION-zoo/sbin/nginx.debug"
+  mv "$INSTALL_PREFIX/nginx-$VERSION$SUFFIX/sbin/nginx" "$INSTALL_PREFIX/nginx-$VERSION$SUFFIX/sbin/nginx.debug"
   cd ..
 }
 
 function build_release() {
-  cd nginx-${VERSION}
-  echo "Configuring release nginx-${VERSION}"
-  ./configure --prefix="$INSTALL_PREFIX/nginx-$VERSION-zoo" \
+  cd nginx-$VERSION
+  echo "Configuring release nginx-$VERSION$SUFFIX"
+  ./configure --prefix="$INSTALL_PREFIX/nginx-$VERSION$SUFFIX" \
+              --with-pcre=$PCRE_PREFIX \
               --with-stream \
               --with-cc-opt="-I$ZOO_PREFIX/include" \
               --with-ld-opt="-L$ZOO_PREFIX/lib -lzookeeper_mt" \
@@ -123,7 +130,7 @@ function build_release() {
     exit $r
   fi
 
-  echo "Build release nginx-${VERSION}-zoo"
+  echo "Build release nginx-$VERSION$SUFFIX"
   make -j 8 > /dev/null
 
   r=$?
@@ -135,7 +142,7 @@ function build_release() {
 }
 
 function download_module() {
-  echo "Download $2"
+  echo "Download $2 branch=$3"
   rm -rf $2 $2-$3
   curl -s -L -O https://github.com/$1/$2/archive/$3.zip
   mv $3.zip $2-$3.zip
@@ -155,21 +162,27 @@ function build_cJSON() {
 }
 
 function download_nginx() {
-  echo "Download nginx-${VERSION}"
-  curl -s -L -O http://nginx.org/download/nginx-${VERSION}.tar.gz
-  tar zxf nginx-${VERSION}.tar.gz -C ../build
+  echo "Download nginx-$VERSION"
+  curl -s -L -O http://nginx.org/download/nginx-$VERSION.tar.gz
+  tar zxf nginx-$VERSION.tar.gz -C ../build
 }
 
 function download_luajit() {
-  echo "Download LuaJIT"
-  curl -s -L -O http://luajit.org/download/LuaJIT-2.1.0-beta2.tar.gz
-  tar zxf LuaJIT-2.1.0-beta2.tar.gz -C ../build
+  echo "Download LuaJIT-$LUAJIT_VERSION"
+  curl -s -L -O http://luajit.org/download/LuaJIT-$LUAJIT_VERSION.tar.gz
+  tar zxf LuaJIT-$LUAJIT_VERSION.tar.gz -C ../build
 }
 
 function download_zoo() {
-  echo "Download Zookeeper"
-  curl -s -L -O http://www-eu.apache.org/dist/zookeeper/zookeeper-3.5.2-alpha/zookeeper-3.5.2-alpha.tar.gz
-  tar zxf zookeeper-3.5.2-alpha.tar.gz -C ../build
+  echo "Download Zookeeper-$ZOO_VERSION"
+  curl -s -L -O http://www-eu.apache.org/dist/zookeeper/zookeeper-$ZOO_VERSION/zookeeper-$ZOO_VERSION.tar.gz
+  tar zxf zookeeper-$ZOO_VERSION.tar.gz -C ../build
+}
+
+function download_pcre() {
+  echo "Download PCRE-$PCRE_VERSION"
+  curl -s -L -O http://ftp.cs.stanford.edu/pub/exim/pcre/pcre-$PCRE_VERSION.tar.gz
+  tar zxf pcre-$PCRE_VERSION.tar.gz -C ../build
 }
 
 function download() {
@@ -190,12 +203,28 @@ function download() {
   download_nginx
   download_luajit
   download_zoo
+  download_pcre
 
   download_module simpl       ngx_devel_kit                    master
   download_module openresty   lua-nginx-module                 master
   download_module openresty   lua-cjson                        master
 
   cd ..
+}
+
+function install_file() {
+  echo "Install $1"
+  if [ ! -e "$INSTALL_PREFIX/nginx-$VERSION$SUFFIX/$2" ]; then
+    mkdir -p "$INSTALL_PREFIX/nginx-$VERSION$SUFFIX/$2"
+  fi
+  cp -r $3 $1 "$INSTALL_PREFIX/nginx-$VERSION$SUFFIX/$2/"
+}
+
+function install_files() {
+  for f in $(ls $1)
+  do
+    install_file $f $2 $3
+  done
 }
 
 function build() {
@@ -221,11 +250,9 @@ function build() {
     build_release
   fi
 
-  cp -r "$JIT_PREFIX/usr/local/lib" "$INSTALL_PREFIX/nginx-$VERSION-zoo"
-  pcrelib=$(ldd "$INSTALL_PREFIX/nginx-$VERSION-zoo/sbin/nginx" | grep pcre | awk '{print $3}')
-  cp $pcrelib "$INSTALL_PREFIX/nginx-$VERSION-zoo/lib"
-  cp lua-cjson/cjson.so "$INSTALL_PREFIX/nginx-$VERSION-zoo/lib/lua/5.1"
-  cp -a $ZOO_PREFIX/lib/*_mt.so* "$INSTALL_PREFIX/nginx-$VERSION-zoo/lib"
+  install_file  "$JIT_PREFIX/usr/local/lib"           .
+  install_file  lua-cjson/cjson.so                    lib/lua/5.1
+  install_files "$ZOO_PREFIX/lib/libzookeeper_mt.so*" lib
 
   cd ..
 }
@@ -236,17 +263,17 @@ build
 
 function install_resty_module() {
   if [ $6 -eq 1 ]; then
-    echo "Download $2"
+    echo "Download $2 branch=$5"
     rm -rf $2-$5 2>/dev/null
     curl -s -L -O https://github.com/$1/$2/archive/$5.zip
     mv $5.zip $2-$5.zip
   fi
   echo "Install $2/$3"
   unzip -q $2-$5.zip
-  if [ ! -e "$INSTALL_PREFIX/nginx-$VERSION-zoo/$4" ]; then
-    mkdir -p "$INSTALL_PREFIX/nginx-$VERSION-zoo/$4"
+  if [ ! -e "$INSTALL_PREFIX/nginx-$VERSION$SUFFIX/$4" ]; then
+    mkdir -p "$INSTALL_PREFIX/nginx-$VERSION$SUFFIX/$4"
   fi
-  cp -r $2-$5/$3 "$INSTALL_PREFIX/nginx-$VERSION-zoo/$4/"
+  cp -r $2-$5/$3 "$INSTALL_PREFIX/nginx-$VERSION$SUFFIX/$4/"
   rm -rf $2-$5
 }
 
@@ -254,8 +281,6 @@ function install_lua_modules() {
   if [ $download -eq 1 ]; then
     rm -rf download/lua_modules/* 2>/dev/null
   fi
-
-  cp -r lua conf "$INSTALL_PREFIX/nginx-$VERSION-zoo"
 
   cd download/lua_modules
 
@@ -267,11 +292,14 @@ function install_lua_modules() {
   install_resty_module ZigzagAK     nginx-resty-auto-healthcheck-config lua/system.lua     lua master 0
 
   cd ../..
+
+  install_file lua  .
+  install_file conf .
 }
 
 install_lua_modules
 
-cp LICENSE "$INSTALL_PREFIX/nginx-$VERSION-zoo/LICENSE"
+cp LICENSE "$INSTALL_PREFIX/nginx-$VERSION$SUFFIX/LICENSE"
 
 cd "$DIR"
 
@@ -279,6 +307,16 @@ kernel_name=$(uname -s)
 kernel_version=$(uname -r)
 
 cd install
-tar zcvf nginx-$VERSION-zoo-$kernel_name-$kernel_version.tar.gz nginx-$VERSION-zoo
-rm -rf nginx-$VERSION-zoo
+tar zcvf nginx-$VERSION$SUFFIX-$kernel_name-$kernel_version.tar.gz nginx-$VERSION$SUFFIX
+
+gunzip -c nginx-$VERSION$SUFFIX-$kernel_name-$kernel_version.tar.gz | tar --list | sort | diff ../t/dist_content.txt -
+r=$?
+if [ $r -eq 0 ]; then
+  rm -rf nginx-$VERSION$SUFFIX
+else
+  rm nginx-$VERSION$SUFFIX-$kernel_name-$kernel_version.tar.gz
+fi
+
 cd ..
+
+exit $r
